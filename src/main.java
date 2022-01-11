5755cd6e56f0c05ed7cf7081cc97b0701f4207ba
Павло Соколюк <pavlosok@i.ua>
2022-01-11 08:37:56 +0200

    zavd 1

---
+++ src/main.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class main {
+
+    public static void main(String[] args){
+        System.out.println("Завдання №1:");
+        List<Integer> listOfIntegers = new ArrayList<>(Arrays.asList(3, 9, 11, 18, 20, 22));
+
+        for (int i=0; i<listOfIntegers.size(); i++)
+        {
+            if (listOfIntegers.get(i)%3==0)
+            {
+                listOfIntegers.remove(i);
+                i--;
+            }
+        }
+        System.out.println(listOfIntegers);
+    }
+
+}
